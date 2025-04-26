ea4bb1a6b77369c9c7b797a3d1976a6b8c3fd5cf
Grace Mtawali <gmtawal@gmail.com>
2025-04-26 16:34:58 -0700

    Added dependencies

---
+++ thriftup/src/main/java/com/example/thriftup/ThriftupApplication.java
@@ -0,0 +1,13 @@
+package com.example.thriftup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThriftupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThriftupApplication.class, args);
+	}
+
+}
---
+++ thriftup/src/test/java/com/example/thriftup/ThriftupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thriftup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThriftupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
