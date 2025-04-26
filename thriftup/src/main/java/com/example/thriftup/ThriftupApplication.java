2bda05e439a286e808e86a5060f37725f65c98e8
Grace Mtawali <gmtawal@gmail.com>
2025-04-26 16:55:51 -0700

    update pom.xml and thriftup on day 1-2

--- thriftup/src/main/java/com/example/thriftup/ThriftupApplication.java
+++ thriftup/src/main/java/com/example/thriftup/ThriftupApplication.java
@@ -2,12 +2,15 @@ package com.example.thriftup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableCaching
+@EnableScheduling
 public class ThriftupApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ThriftupApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ThriftupApplication.class, args);
+    }
 }
